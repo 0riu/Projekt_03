7d077b9ba15d377951a010960135cf42d5229da1
0riu <matfir01@gmail.com>
2024-12-03 14:38:35 +0100

    Final version with matrix adjustment

--- Assets/Scripts/Transformacje2D.cs
+++ Assets/Scripts/Transformacje2D.cs
@@ -17,70 +17,77 @@ public class Transformacje2D : MonoBehaviour
 
     void Update()
     {
-        // Obrót S1 wokół własnej osi
+        // Obrót S1 wokół własnej osi za pomocą macierzy
         RotateUsingMatrix(S1, rotationSpeedS1);
 
-        // Obieg S2 wokół S1, z zachowaniem orientacji względem S1
-        OrbitAndAlignLongSide(S2, S1, ref currentAngleS2, orbitRadiusS2, orbitSpeedS2);
+        // Obieg S2 wokół S1 za pomocą macierzy
+        OrbitAndAlignUsingMatrix(S2, S1, ref currentAngleS2, orbitRadiusS2, orbitSpeedS2);
 
-        // Obieg S3 wokół S2, z zachowaniem orientacji względem S2
-        OrbitAndAlignLongSide(S3, S2, ref currentAngleS3, orbitRadiusS3, orbitSpeedS3);
+        // Obieg S3 wokół S2 za pomocą macierzy
+        OrbitAndAlignUsingMatrix(S3, S2, ref currentAngleS3, orbitRadiusS3, orbitSpeedS3);
     }
 
     void RotateUsingMatrix(Transform obj, float speed)
     {
+        // Kąt obrotu dla bieżącej klatki
         float angle = speed * Time.deltaTime;
+
+        // Macierz rotacji
         Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 0, angle));
+
+        // Obrót obiektu (pozycja i orientacja)
         obj.position = rotationMatrix.MultiplyPoint3x4(obj.position - obj.position) + obj.position;
         obj.rotation *= Quaternion.Euler(0, 0, angle);
     }
 
-    void OrbitAndAlignLongSide(Transform obj, Transform center, ref float currentAngle, float radius, float speed)
+    void OrbitAndAlignUsingMatrix(Transform obj, Transform center, ref float currentAngle, float radius, float speed)
     {
+        // Aktualizacja kąta orbity
         currentAngle += speed * Time.deltaTime;
         currentAngle %= 360f;
 
-        float radian = currentAngle * Mathf.Deg2Rad;
-        Vector3 localOffset = new Vector3(
-            Mathf.Cos(radian) * radius,
-            Mathf.Sin(radian) * radius,
-            0
-        );
+        // Tworzenie macierzy transformacji dla orbity
+        Matrix4x4 translationMatrix = Matrix4x4.Translate(center.position);
+        Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 0, currentAngle));
+        Matrix4x4 orbitMatrix = translationMatrix * rotationMatrix;
 
-        obj.position = center.position + localOffset;
-        obj.rotation = center.rotation * Quaternion.Euler(0, 0, 90f); // Zachowanie orientacji
+        // Pozycja na orbicie
+        Vector3 localOffset = new Vector3(radius, 0, 0);
+        Vector3 newPosition = orbitMatrix.MultiplyPoint3x4(localOffset);
+
+        // Ustawienie pozycji
+        obj.position = newPosition;
+
+        // Zachowanie orientacji względem centrum
+        obj.rotation = center.rotation * Quaternion.Euler(0, 0, 90f);
     }
 
     private void OnValidate()
     {
-        // Początkowy kąt orbity
-        currentAngleS2 = 0f;
-        currentAngleS3 = 0f;
-
-        // Ustaw pozycję i orientację S2 względem S1 za pomocą macierzy
+        // Ustawienie pozycji i orientacji S2 względem S1 za pomocą macierzy
         if (S1 != null && S2 != null)
         {
             Matrix4x4 translationMatrix = Matrix4x4.Translate(S1.position);
-            Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 0, currentAngleS2));
+            Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 0, 0)); // Początkowy kąt
             Matrix4x4 orbitMatrix = translationMatrix * rotationMatrix;
 
             Vector3 localOffset = new Vector3(orbitRadiusS2, 0, 0);
             S2.position = orbitMatrix.MultiplyPoint3x4(localOffset);
 
-            S2.rotation = S1.rotation * Quaternion.Euler(0, 0, 90f); // Zachowanie orientacji
+            S2.rotation = S1.rotation * Quaternion.Euler(0, 0, 90f);
         }
 
-        // Ustaw pozycję i orientację S3 względem S2 za pomocą macierzy
+        // Ustawienie pozycji i orientacji S3 względem S2 za pomocą macierzy
         if (S2 != null && S3 != null)
         {
             Matrix4x4 translationMatrix = Matrix4x4.Translate(S2.position);
-            Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 0, currentAngleS3));
+            Matrix4x4 rotationMatrix = Matrix4x4.Rotate(Quaternion.Euler(0, 0, 0)); // Początkowy kąt
             Matrix4x4 orbitMatrix = translationMatrix * rotationMatrix;
 
             Vector3 localOffset = new Vector3(orbitRadiusS3, 0, 0);
             S3.position = orbitMatrix.MultiplyPoint3x4(localOffset);
 
-            S3.rotation = S2.rotation * Quaternion.Euler(0, 0, 90f); // Zachowanie orientacji
+            S3.rotation = S2.rotation * Quaternion.Euler(0, 0, 90f);
         }
     }
 }
